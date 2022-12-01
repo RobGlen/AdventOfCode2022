d1ae5821e098e57315697c50d4e17152e3576527
Steam Deck User <deck@steamdeck.home>
2022-12-01 20:50:42 +0000

    Removing a magic number.

--- Source/Days/Day01.c
+++ Source/Days/Day01.c
@@ -34,13 +34,14 @@ void ExecuteDay01_Part1(struct DayData* dayData)
 
 void ExecuteDay01_Part2(struct DayData* dayData)
 {
+    const int numOfLargest = 3;
     int count = 0;
     int largestCount[3] = {0, 0, 0};
 
     for (int i = 0; i < dayData->m_DataLength; ++i)
     {
         const int isEmptyString = strcmp(dayData->m_Data[i], "") == 0;
-        
+
         if (!isEmptyString)
         {
             const int dataToNum = atoi(dayData->m_Data[i]);
@@ -50,7 +51,7 @@ void ExecuteDay01_Part2(struct DayData* dayData)
         // only one case where this can still execute after the previous if, still worth not else-ing
         if (isEmptyString || (i == dayData->m_DataLength - 1))
         {
-            for (int j = 0; j < 3; ++j)
+            for (int j = 0; j < numOfLargest; ++j)
             {
                 if (largestCount[j] < count)
                 {
@@ -74,7 +75,7 @@ void ExecuteDay01_Part2(struct DayData* dayData)
     }
 
     int totalCount = 0;
-    for (int i = 0; i < 3; ++i)
+    for (int i = 0; i < numOfLargest; ++i)
     {
         totalCount += largestCount[i];
     }
