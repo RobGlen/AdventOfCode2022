2200fa2032762a7a09bbb59638406fda80f91e20
Rob Glen <deck@steamdeck.home>
2022-12-04 13:37:25 +0000

    Cleaning up day 4 a bit, reusing code instead of copy and paste.

--- Source/Days/Day04.c
+++ Source/Days/Day04.c
@@ -6,44 +6,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void DetermineRanges(char* const sourceStr, int* ranges)
+{
+    int i = 0;
+    int commaIndex = 0;
+    int hyphenIndex[2] = {0, 0};
+    int hyphenToSet = 0;
+    while(sourceStr[i] != '\0')
+    {
+        if (sourceStr[i] == '-')
+        {
+            hyphenIndex[hyphenToSet] = i;
+            hyphenToSet++;
+        }
+        else if (sourceStr[i] == ',')
+        {
+            commaIndex = i;
+        }
+        ++i;
+    }
+
+    char str[20];
+    strcpy(str, sourceStr);
+    strtok(str, ",\0");
+    strtok(str, "-\0");
+    strtok(str + commaIndex + 1, "-\0");
+    
+    ranges[0] = atoi(str);
+    ranges[1] = atoi(str + hyphenIndex[0] + 1);
+    ranges[2] = atoi(str + commaIndex + 1);
+    ranges[3] = atoi(str + hyphenIndex[1] + 1);
+}
+
 void ExecuteDay04_Part1(struct DayData* dayData)
 { 
     int countOfOverlap = 0;
     for (int i = 0; i < dayData->m_DataLength; ++i)
     {
-        dayData->m_Data[i];
-
-        int j = 0;
-        int commaIndex = 0;
-        int hyphenIndex[2] = {0, 0};
-        int hyphenToSet = 0;
-        while(dayData->m_Data[i][j] != '\0')
-        {
-            if (dayData->m_Data[i][j] == '-')
-            {
-                hyphenIndex[hyphenToSet] = j;
-                hyphenToSet++;
-            }
-            else if (dayData->m_Data[i][j] == ',')
-            {
-                commaIndex = j;
-            }
-            ++j;
-        }
-
-        char str[20];
-        char* strptr = str;
-        strcpy(str, dayData->m_Data[i]);
-        strtok(str, ",\0");
-        strtok(str, "-\0");
-        strtok(strptr + commaIndex + 1, "-\0");
-        int ranges[4] = 
-        {
-            atoi(str),
-            atoi(str + hyphenIndex[0] + 1),
-            atoi(str + commaIndex + 1),
-            atoi(str + hyphenIndex[1] + 1),
-        };
+        int ranges[4];
+        DetermineRanges(dayData->m_Data[i], ranges);
 
         if (((ranges[0] <= ranges[2]) && (ranges[1] >= ranges[3])) ||
             ((ranges[0] >= ranges[2]) && (ranges[1] <= ranges[3])))
@@ -56,42 +57,11 @@ void ExecuteDay04_Part1(struct DayData* dayData)
 
 void ExecuteDay04_Part2(struct DayData* dayData)
 {
-        int countOfOverlap = 0;
+    int countOfOverlap = 0;
     for (int i = 0; i < dayData->m_DataLength; ++i)
     {
-        dayData->m_Data[i];
-
-        int j = 0;
-        int commaIndex = 0;
-        int hyphenIndex[2] = {0, 0};
-        int hyphenToSet = 0;
-        while(dayData->m_Data[i][j] != '\0')
-        {
-            if (dayData->m_Data[i][j] == '-')
-            {
-                hyphenIndex[hyphenToSet] = j;
-                hyphenToSet++;
-            }
-            else if (dayData->m_Data[i][j] == ',')
-            {
-                commaIndex = j;
-            }
-            ++j;
-        }
-
-        char str[20];
-        char* strptr = str;
-        strcpy(str, dayData->m_Data[i]);
-        strtok(str, ",\0");
-        strtok(str, "-\0");
-        strtok(strptr + commaIndex + 1, "-\0");
-        int ranges[4] = 
-        {
-            atoi(str),
-            atoi(str + hyphenIndex[0] + 1),
-            atoi(str + commaIndex + 1),
-            atoi(str + hyphenIndex[1] + 1),
-        };
+        int ranges[4];
+        DetermineRanges(dayData->m_Data[i], ranges);
 
         if (((ranges[0] <= ranges[2]) && (ranges[1] >= ranges[3])) ||
             ((ranges[0] >= ranges[2]) && (ranges[1] <= ranges[3])) ||
