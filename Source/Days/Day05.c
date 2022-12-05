c95e077703ec40f61a070f107bd83fc3860e831c
Robert Glen <robert.glen@splashdamage.com>
2022-12-05 17:21:30 +0000

    Converting tabs to spaces as there was a mix from moving to a different setup.

--- Source/Days/Day05.c
+++ Source/Days/Day05.c
@@ -42,7 +42,6 @@ void ParseCrates(DayData* const dayData, Crate** crateStack, Crate* allCrates, c
     {
         dayData->m_Data[i];
         j = 1;
-     /*   int stackIndex = 0;*/
 
         for (j = 0; j < 9; ++j)
         {
@@ -53,24 +52,24 @@ void ParseCrates(DayData* const dayData, Crate** crateStack, Crate* allCrates, c
                 continue;
             }
 
-			allCrates[crateIndex].id = dayData->m_Data[i][stackIndex];
-			if (crateStack[j] != NULL)
-			{
-				Crate* currentCrate = crateStack[j];
-				while (currentCrate->next != NULL)
-				{
-					currentCrate = currentCrate->next;
-				}
+            allCrates[crateIndex].id = dayData->m_Data[i][stackIndex];
+            if (crateStack[j] != NULL)
+            {
+                Crate* currentCrate = crateStack[j];
+                while (currentCrate->next != NULL)
+                {
+                    currentCrate = currentCrate->next;
+                }
 
-				currentCrate->next = &allCrates[crateIndex];
-				currentCrate->next->prev = currentCrate;
-				crateIndex++;
-			}
+                currentCrate->next = &allCrates[crateIndex];
+                currentCrate->next->prev = currentCrate;
+                crateIndex++;
+            }
             else
             {
                 crateStack[j] = &allCrates[crateIndex];
-				crateIndex++;
-			}
+                crateIndex++;
+            }
 
         }
     }
@@ -85,21 +84,21 @@ void ParseInstructions(DayData* dayData, Crate** crateStack, int instructionsSta
         int j = 0;
         for (int k = 0; k < 3; ++k)
         {
-			while (dayData->m_Data[i][j] != ' ')
-			{
+            while (dayData->m_Data[i][j] != ' ')
+            {
                 ++j;
-			}
+            }
 
-			++j;
+            ++j;
 
-			int len = j;
+            int len = j;
 
             char str[4];
-			while (dayData->m_Data[i][len] != ' ' && dayData->m_Data[i][len] != '\0')
-			{
+            while (dayData->m_Data[i][len] != ' ' && dayData->m_Data[i][len] != '\0')
+            {
                 str[len - j] = dayData->m_Data[i][len];
                 ++len;
-			}
+            }
 
             str[len - j] = '\0';
 
@@ -114,160 +113,160 @@ void ParseInstructions(DayData* dayData, Crate** crateStack, int instructionsSta
         const int moveFrom = rules[1] - 1;
         const int moveTo = rules[2] - 1;
 
-		Crate* crateToMove = crateStack[moveFrom];
+        Crate* crateToMove = crateStack[moveFrom];
 
         if (crateToMove == NULL)
         {
             continue;
         }
 
-		while (crateToMove->next != NULL)
-		{
-			crateToMove = crateToMove->next;
-		}
+        while (crateToMove->next != NULL)
+        {
+            crateToMove = crateToMove->next;
+        }
         
         if (shouldKeepOrder)
         {
-			Crate* startCrate = crateToMove;
-			for (j = 0; j < moveAmount - 1; ++j)
-			{
-				if (startCrate != NULL)
-				{
-					startCrate = startCrate->prev;
-				}
-			}
+            Crate* startCrate = crateToMove;
+            for (j = 0; j < moveAmount - 1; ++j)
+            {
+                if (startCrate != NULL)
+                {
+                    startCrate = startCrate->prev;
+                }
+            }
 
-			Crate* prevCrateInMoveStack = startCrate->prev;
+            Crate* prevCrateInMoveStack = startCrate->prev;
 
-			if (prevCrateInMoveStack != NULL)
-			{
-				prevCrateInMoveStack->next = NULL;
-			}
+            if (prevCrateInMoveStack != NULL)
+            {
+                prevCrateInMoveStack->next = NULL;
+            }
 
-			Crate* crateTarget = crateStack[moveTo];
+            Crate* crateTarget = crateStack[moveTo];
 
-			if (crateStack[moveTo] == NULL)
-			{
-				crateStack[moveTo] = startCrate;
-			}
-			else
-			{
-				while (crateTarget->next != NULL)
-				{
-					crateTarget = crateTarget->next;
-				}
+            if (crateStack[moveTo] == NULL)
+            {
+                crateStack[moveTo] = startCrate;
+            }
+            else
+            {
+                while (crateTarget->next != NULL)
+                {
+                    crateTarget = crateTarget->next;
+                }
 
-				crateTarget->next = startCrate;
-			}
+                crateTarget->next = startCrate;
+            }
 
-			startCrate->prev = crateTarget;
-			crateToMove->next = NULL;
+            startCrate->prev = crateTarget;
+            crateToMove->next = NULL;
 
-			if (startCrate == crateStack[moveFrom])
-			{
-				crateStack[moveFrom] = NULL;
-			}
+            if (startCrate == crateStack[moveFrom])
+            {
+                crateStack[moveFrom] = NULL;
+            }
         }
         else
         {
-			for (j = 0; j < moveAmount; ++j)
-			{
-				Crate* prevCrateInMoveStack = crateToMove->prev;
+            for (j = 0; j < moveAmount; ++j)
+            {
+                Crate* prevCrateInMoveStack = crateToMove->prev;
 
-				if (prevCrateInMoveStack != NULL)
-				{
-					prevCrateInMoveStack->next = NULL;
-				}
+                if (prevCrateInMoveStack != NULL)
+                {
+                    prevCrateInMoveStack->next = NULL;
+                }
 
-				Crate* crateTarget = crateStack[moveTo];
+                Crate* crateTarget = crateStack[moveTo];
 
-				if (crateStack[moveTo] == NULL)
-				{
-					crateStack[moveTo] = crateToMove;
-				}
-				else
-				{
-					while (crateTarget->next != NULL)
-					{
-						crateTarget = crateTarget->next;
-					}
+                if (crateStack[moveTo] == NULL)
+                {
+                    crateStack[moveTo] = crateToMove;
+                }
+                else
+                {
+                    while (crateTarget->next != NULL)
+                    {
+                        crateTarget = crateTarget->next;
+                    }
 
-					crateTarget->next = crateToMove;
-				}
+                    crateTarget->next = crateToMove;
+                }
 
-				crateToMove->prev = crateTarget;
-				crateToMove->next = NULL;
+                crateToMove->prev = crateTarget;
+                crateToMove->next = NULL;
 
-				if (crateToMove == crateStack[moveFrom])
-				{
-					crateStack[moveFrom] = NULL;
-				}
+                if (crateToMove == crateStack[moveFrom])
+                {
+                    crateStack[moveFrom] = NULL;
+                }
 
-				crateToMove = prevCrateInMoveStack;
+                crateToMove = prevCrateInMoveStack;
 
-				if (crateToMove == NULL)
-				{
-					break;
-				}
-			}
+                if (crateToMove == NULL)
+                {
+                    break;
+                }
+            }
         }
     }
 }
 
 void SortoutCrates(DayData* dayData, BOOL shouldKeepOrder)
 {
-	Crate* crateStack[9];
-	Crate allCrates[65];
+    Crate* crateStack[9];
+    Crate allCrates[65];
 
-	for (int i = 0; i < 65; ++i)
-	{
-		allCrates[i].id = 0;
-		allCrates[i].next = NULL;
-		allCrates[i].prev = NULL;
-	}
+    for (int i = 0; i < 65; ++i)
+    {
+        allCrates[i].id = 0;
+        allCrates[i].next = NULL;
+        allCrates[i].prev = NULL;
+    }
 
-	for (int i = 0; i < 9; ++i)
-	{
-		crateStack[i] = NULL;
-	}
-	int instructionsIndex = 0;
-	ParseCrates(dayData, crateStack, allCrates, 65, &instructionsIndex);
+    for (int i = 0; i < 9; ++i)
+    {
+        crateStack[i] = NULL;
+    }
+    int instructionsIndex = 0;
+    ParseCrates(dayData, crateStack, allCrates, 65, &instructionsIndex);
 
-	for (int i = 0; i < 9; ++i)
-	{
-		Crate* crate = crateStack[i];
-		while (crate != NULL)
-		{
-			printf("[%c]", crate->id);
-			crate = crate->next;
-		}
-		printf("\n");
-	}
+    for (int i = 0; i < 9; ++i)
+    {
+        Crate* crate = crateStack[i];
+        while (crate != NULL)
+        {
+            printf("[%c]", crate->id);
+            crate = crate->next;
+        }
+        printf("\n");
+    }
 
-	ParseInstructions(dayData, crateStack, instructionsIndex, shouldKeepOrder);
+    ParseInstructions(dayData, crateStack, instructionsIndex, shouldKeepOrder);
 
-	for (int i = 0; i < 9; ++i)
-	{
-		Crate* crate = crateStack[i];
-		if (crate != NULL)
-		{
-			while (crate->next != NULL)
-			{
-				crate = crate->next;
-			}
-			printf("%c", crate->id);
-		}
-	}
+    for (int i = 0; i < 9; ++i)
+    {
+        Crate* crate = crateStack[i];
+        if (crate != NULL)
+        {
+            while (crate->next != NULL)
+            {
+                crate = crate->next;
+            }
+            printf("%c", crate->id);
+        }
+    }
 
-	printf("\n");
+    printf("\n");
 }
 
 void ExecuteDay05_Part1(DayData* dayData)
 { 
-	SortoutCrates(dayData, FALSE);
+    SortoutCrates(dayData, FALSE);
 }
 
 void ExecuteDay05_Part2(DayData* dayData)
 {
-	SortoutCrates(dayData, TRUE);
+    SortoutCrates(dayData, TRUE);
 }
