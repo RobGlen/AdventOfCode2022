97c8f0ee2ddf8d314516eb10746e3f3c92bad7b7
Rob Glen <deck@steamdeck.home>
2022-12-14 18:58:20 +0000

    Adding macro for look up so the index used to set the func ptrs is the same number as the day.

--- Source/DayLookup.c
+++ Source/DayLookup.c
@@ -17,6 +17,8 @@
 #include "Days/Day12.h"
 #include "Days/Day13.h"
 
+#define DAY(i) i - 1
+
 DayFuncPair* CreateDayFuncPairList()
 {
     DayFuncPair* dayFuncPairs = malloc(sizeof(DayFuncPair) * NUM_OF_DAYS);
@@ -27,44 +29,44 @@ DayFuncPair* CreateDayFuncPairList()
         dayFuncPairs[i].m_dayFunc2 = NULL;
     }
 
-    dayFuncPairs[0].m_dayFunc1 = &ExecuteDay01_Part1;
-    dayFuncPairs[0].m_dayFunc2 = &ExecuteDay01_Part2;
+    dayFuncPairs[DAY(1)].m_dayFunc1 = &ExecuteDay01_Part1;
+    dayFuncPairs[DAY(1)].m_dayFunc2 = &ExecuteDay01_Part2;
 
-    dayFuncPairs[1].m_dayFunc1 = &ExecuteDay02_Part1;
-    dayFuncPairs[1].m_dayFunc2 = &ExecuteDay02_Part2;
+    dayFuncPairs[DAY(2)].m_dayFunc1 = &ExecuteDay02_Part1;
+    dayFuncPairs[DAY(2)].m_dayFunc2 = &ExecuteDay02_Part2;
 
-    dayFuncPairs[2].m_dayFunc1 = &ExecuteDay03_Part1;
-    dayFuncPairs[2].m_dayFunc2 = &ExecuteDay03_Part2;
+    dayFuncPairs[DAY(3)].m_dayFunc1 = &ExecuteDay03_Part1;
+    dayFuncPairs[DAY(3)].m_dayFunc2 = &ExecuteDay03_Part2;
 
-    dayFuncPairs[3].m_dayFunc1 = &ExecuteDay04_Part1;
-    dayFuncPairs[3].m_dayFunc2 = &ExecuteDay04_Part2;
+    dayFuncPairs[DAY(4)].m_dayFunc1 = &ExecuteDay04_Part1;
+    dayFuncPairs[DAY(4)].m_dayFunc2 = &ExecuteDay04_Part2;
 
-    dayFuncPairs[4].m_dayFunc1 = &ExecuteDay05_Part1;
-    dayFuncPairs[4].m_dayFunc2 = &ExecuteDay05_Part2;
+    dayFuncPairs[DAY(5)].m_dayFunc1 = &ExecuteDay05_Part1;
+    dayFuncPairs[DAY(5)].m_dayFunc2 = &ExecuteDay05_Part2;
 
-    dayFuncPairs[5].m_dayFunc1 = &ExecuteDay06_Part1;
-    dayFuncPairs[5].m_dayFunc2 = &ExecuteDay06_Part2;
+    dayFuncPairs[DAY(6)].m_dayFunc1 = &ExecuteDay06_Part1;
+    dayFuncPairs[DAY(6)].m_dayFunc2 = &ExecuteDay06_Part2;
 
-    dayFuncPairs[6].m_dayFunc1 = &ExecuteDay07_Part1;
-    dayFuncPairs[6].m_dayFunc2 = &ExecuteDay07_Part2;
+    dayFuncPairs[DAY(7)].m_dayFunc1 = &ExecuteDay07_Part1;
+    dayFuncPairs[DAY(7)].m_dayFunc2 = &ExecuteDay07_Part2;
 
-    dayFuncPairs[7].m_dayFunc1 = &ExecuteDay08_Part1;
-    dayFuncPairs[7].m_dayFunc2 = &ExecuteDay08_Part2;
+    dayFuncPairs[DAY(8)].m_dayFunc1 = &ExecuteDay08_Part1;
+    dayFuncPairs[DAY(8)].m_dayFunc2 = &ExecuteDay08_Part2;
 
-	dayFuncPairs[8].m_dayFunc1 = &ExecuteDay09_Part1;
-	dayFuncPairs[8].m_dayFunc2 = &ExecuteDay09_Part2;
+	dayFuncPairs[DAY(9)].m_dayFunc1 = &ExecuteDay09_Part1;
+	dayFuncPairs[DAY(9)].m_dayFunc2 = &ExecuteDay09_Part2;
 
-    dayFuncPairs[9].m_dayFunc1 = &ExecuteDay10_Part1;
-	dayFuncPairs[9].m_dayFunc2 = &ExecuteDay10_Part2;
+    dayFuncPairs[DAY(10)].m_dayFunc1 = &ExecuteDay10_Part1;
+	dayFuncPairs[DAY(10)].m_dayFunc2 = &ExecuteDay10_Part2;
 
-    dayFuncPairs[10].m_dayFunc1 = &ExecuteDay11_Part1;
-	dayFuncPairs[10].m_dayFunc2 = &ExecuteDay11_Part2;
+    dayFuncPairs[DAY(11)].m_dayFunc1 = &ExecuteDay11_Part1;
+	dayFuncPairs[DAY(11)].m_dayFunc2 = &ExecuteDay11_Part2;
 
-    dayFuncPairs[11].m_dayFunc1 = &ExecuteDay12_Part1;
-	dayFuncPairs[11].m_dayFunc2 = &ExecuteDay12_Part2;
+    dayFuncPairs[DAY(12)].m_dayFunc1 = &ExecuteDay12_Part1;
+	dayFuncPairs[DAY(12)].m_dayFunc2 = &ExecuteDay12_Part2;
 
-    dayFuncPairs[12].m_dayFunc1 = &ExecuteDay13_Part1;
-	dayFuncPairs[12].m_dayFunc2 = &ExecuteDay13_Part2;
+    dayFuncPairs[DAY(13)].m_dayFunc1 = &ExecuteDay13_Part1;
+	dayFuncPairs[DAY(13)].m_dayFunc2 = &ExecuteDay13_Part2;
 
     return dayFuncPairs;
 }
@@ -76,3 +78,5 @@ void FreeDayFuncPairList(DayFuncPair* const dayFuncPairList)
         free(dayFuncPairList);
     }
 }
+
+#undef DAY
