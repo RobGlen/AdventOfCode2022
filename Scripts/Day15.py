df221c8a6ea4c54ab0e30f11f9c10e55d33da9e7
Rob Glen <deck@steamdeck.home>
2022-12-15 22:50:24 +0000

    Starting day 15.

---
+++ Scripts/Day15.py
@@ -0,0 +1,14 @@
+class Day15():
+    def __init__(self):
+        file = open("Data/Input/Day15.txt")
+        self.data = [line for line in file]
+        file.close()
+
+    def ExecuteDayPart1(self):
+        pass
+
+    def ExecuteDayPart2(self):
+        pass
+
+day = Day15()
+day.ExecuteDayPart1()
